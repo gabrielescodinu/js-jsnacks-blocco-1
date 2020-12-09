1b45d4f66265746b5571992f3df4655748b0e1d8
gabriele <gabrielescodinu96@gmail.com>
2020-12-09 12:18:33 +0100

    commit 03

--- main5.js
+++ main5.js
@@ -2,17 +2,8 @@
 // argomento e la ritorni girata (es. Ciao -> oaiC)
 
 function reverseString(str) {
-  // uso split() e mi divide le lettere creando un array
-  var splitString = str.split("");
-
-  // uso reverse() per rigirare l'array appena creato
-  var reverse = splitString.reverse();
-
-  // uso join() per unire gli elementi dell'array in un'unica stringa
-  var joinArray = reverse.join("");
-
-  // restituisco a return l'array completo
-  return joinArray;
+  var splitString = str.split("").reverse().join("");
+  return splitString;
 }
 
 var parola = "ciao";
